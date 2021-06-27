223cf6bc7eeddd24369ea9ec39316c3c1998f63c
Andrj3 <Andrj3@bfh.ch>
2021-06-27 16:39:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -194,13 +194,15 @@ with clustering:
     recommendedNumberOfClustersIndex = silhouetteScore_df.idxmax(axis= 0, skipna=True)[1]
     recommendedNumberOfClusters = silhouetteScore_df.numberOfClusters[recommendedNumberOfClustersIndex]
 
-    #fig, ax = plt.subplots()
-    #fig = sns.lineplot(
-    #    data = silhouetteScore_df,
-    #    x = 'numberOfClusters',
-    #    y = 'SilhouetteScore'
-    #    )
-    #silcoeff_col.pyplot(fig)
+    fig, ax = plt.subplots(1,2)
+
+    fig = sns.lineplot(
+        data = silhouetteScore_df,
+        x = 'numberOfClusters',
+        y = 'SilhouetteScore',
+        ax = ax[0]
+        )
+    st.pyplot(fig)
 
 ### 3.2.2 - elbow-Method ### 
     k_rng = range(1,maxNumberOfClusters)
@@ -210,10 +212,11 @@ with clustering:
         km.fit(df_normalized[clusteringAttributesLst])
         sse_scaler.append(km.inertia_)
 
-    #fig = sns.lineplot(
-    #    data = sse_scaler
-    #    )
-    #elbow_col.pyplot(fig)
+    fig = sns.lineplot(
+        data = sse_scaler,
+        ax = [1]
+        )
+    st.pyplot(fig)
 
 ### 3.2.2 - def reccomendation ### 
     chosenNumberOfClusters = st.slider('How many do you whish?', min_value=2, max_value=20, value= int(recommendedNumberOfClusters), step= 1)
@@ -275,32 +278,3 @@ with visualization:
         palette = sns.color_palette("Set2", numberOfClusters),
         )
     st.pyplot(fig)
-
-
-
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import streamlit as st
-    import time
-
-    fig, ax = plt.subplots()
-
-    max_x = 5
-    max_rand = 10
-
-    x = np.arange(0, max_x)
-    ax.set_ylim(0, max_rand)
-    line, = ax.plot(x, np.random.randint(0, max_rand, max_x))
-    the_plot = st.pyplot(plt)
-
-    def init():  # give a clean slate to start
-        line.set_ydata([np.nan] * len(x))
-
-    def animate(i):  # update the y values (every 1000ms)
-        line.set_ydata(np.random.randint(0, max_rand, max_x))
-        the_plot.pyplot(plt)
-
-    init()
-    for i in range(100):
-        animate(i)
-        time.sleep(0.1)
